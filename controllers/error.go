6fb55da2f1daa3cbaa7d826c81963dc3556285d3
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2019-03-28 18:24:18 +0200

    [EPMDEDP-2290] create 404 page
    Change-Id: I3c277298ec7ba4d526a0a6f9dead016e8d8c4021

--- controllers/error.go
+++ controllers/error.go
@@ -7,9 +7,13 @@ type ErrorController struct {
 }
 
 func (this *ErrorController) Error500() {
-	this.TplName = "error_500.html"
+	this.TplName = "error/error_500.html"
 }
 
 func (this *ErrorController) Error403() {
-	this.TplName = "error_403.html"
+	this.TplName = "error/error_403.html"
+}
+
+func (this *ErrorController) Error404() {
+	this.TplName = "error/error_404.html"
 }
