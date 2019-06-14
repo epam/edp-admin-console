8bdda6adf9dda9a7a1d60199b262674b217a32ae
Oleksiy Luchkovskiy <oleksiy_luchkovskiy@epam.com>
2019-06-14 10:44:57 +0300

    [EPMDEDP-3096] Create an alert pop-up badge
    Change-Id: Ieb66383ede8fa15c137fceeb697497202657b624

--- static/js/codebase-overview.js
+++ static/js/codebase-overview.js
@@ -11,7 +11,19 @@ $(function () {
         let anchor = $(location).attr('hash');
         if (anchor) {
             if (anchor === '#codebaseSuccessModal') {
-                $('#successPopup').modal('show');
+                // $('#successPopup').modal('show');
+                $.notify({
+                        icon: 'glyphicon glyphicon-ok-circle alert-icon',
+                        message: "Provisioning has been started."
+                    },
+                    {
+                        type: 'success',
+                        delay: 10000,
+                        animate: {
+                            enter:'animated fadeInRight',
+                            exit: 'animated fadeOutRight'
+                        }
+                    });
             }
             location.hash = '';
         }
@@ -20,7 +32,39 @@ $(function () {
         let codebaseName = getUrlParameter('waitingforcodebase');
         if (codebaseName) {
             let status = $("tr[data-codebase-name='" + codebaseName + "']").attr("data-codebase-status");
-            if (status !== STATUS.CREATED && status !== STATUS.FAILED) {
+
+            if (status == STATUS.CREATED)
+            {
+                $.notify({
+                        icon: 'glyphicon glyphicon-ok-circle alert-icon',
+                        message: "Provisioning has been finished."
+                    },
+                    {
+                        type: 'success',
+                        delay: 5000,
+                        animate: {
+                            enter:'animated fadeInRight',
+                            exit: 'animated fadeOutRight'
+                        }
+                    });
+            }
+            else
+            if (status == STATUS.FAILED) {
+                $.notify({
+                        icon: 'glyphicon gglyphicon-warning-sign alert-icon',
+                        message: "Provisioning has been failed."
+                    },
+                    {
+                        type: 'error',
+                        delay: 5000,
+                        animate: {
+                            enter:'animated fadeInRight',
+                            exit: 'animated fadeOutRight'
+                        }
+                    });
+
+            }
+            else {
                 uri += "?waitingforcodebase=" + codebaseName;
                 setTimeout(function () {
                     location.reload();
