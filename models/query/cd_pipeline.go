def0457d2d59568697becb818d70acf44498c9c7
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2019-06-26 12:59:45 +0300

    [EPMDEDP-3299] Remove CodebaseStageMatrix from REST Api response
    Change-Id: I340bf931647c61dbd105d78ba203aefab0586e19

--- models/query/cd_pipeline.go
+++ models/query/cd_pipeline.go
@@ -1,19 +1,19 @@
 package query
 
 type CDPipeline struct {
-	Id                int                  `json:"id" orm:"column(id)"`
-	Name              string               `json:"name" orm:"column(name)"`
-	Status            string               `json:"status" orm:"column(status)"`
-	JenkinsLink       string               `json:"jenkinsLink" orm:"-"`
-	CodebaseBranch    []*CodebaseBranch    `json:"codebaseBranches" orm:"rel(m2m);rel_table(cd_pipeline_codebase_branch)"`
-	Stage             []*Stage             `json:"stages" orm:"reverse(many)"`
-	ThirdPartyService []*ThirdPartyService `json:"services" orm:"rel(m2m);rel_table(cd_pipeline_third_party_service)"`
-	CodebaseStageMatrix map[CDCodebaseStageMatrixKey]CDCodebaseStageMatrixValue `json:"codebaseStageMatrix" orm:"-"`
+	Id                  int                                                     `json:"id" orm:"column(id)"`
+	Name                string                                                  `json:"name" orm:"column(name)"`
+	Status              string                                                  `json:"status" orm:"column(status)"`
+	JenkinsLink         string                                                  `json:"jenkinsLink" orm:"-"`
+	CodebaseBranch      []*CodebaseBranch                                       `json:"codebaseBranches" orm:"rel(m2m);rel_table(cd_pipeline_codebase_branch)"`
+	Stage               []*Stage                                                `json:"stages" orm:"reverse(many)"`
+	ThirdPartyService   []*ThirdPartyService                                    `json:"services" orm:"rel(m2m);rel_table(cd_pipeline_third_party_service)"`
+	CodebaseStageMatrix map[CDCodebaseStageMatrixKey]CDCodebaseStageMatrixValue `json:"-" orm:"-"`
 }
 
 type CDCodebaseStageMatrixKey struct {
 	CodebaseBranch *CodebaseBranch `json:"codebaseBranch"`
-	Stage *Stage `json:"stage"`
+	Stage          *Stage          `json:"stage"`
 }
 
 type CDCodebaseStageMatrixValue struct {
@@ -23,10 +23,9 @@ type CDCodebaseStageMatrixValue struct {
 func (c *CDPipeline) GetCDCodebaseStageMatrixValue(codebase *CodebaseBranch, stage *Stage) CDCodebaseStageMatrixValue {
 	return c.CodebaseStageMatrix[CDCodebaseStageMatrixKey{
 		CodebaseBranch: codebase,
-		Stage:     stage,
+		Stage:          stage,
 	}]
 }
-
 
 type CDPipelineCriteria struct {
 	Status Status
