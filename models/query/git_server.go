8d1251fa634f9bded3811fae5f2a7c7f816dae14
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2019-09-12 11:40:29 +0300

    [EPMDEDP-3632] Display hostname on UI
    Change-Id: Ia497905819eca85cd9f09a55dde65fe6748121b9

--- models/query/git_server.go
+++ models/query/git_server.go
@@ -3,6 +3,7 @@ package query
 type GitServer struct {
 	Id        int    `json:"id" orm:"column(id)"`
 	Name      string `json:"name" orm:"column(name)"`
+	Hostname  string `json:"hostname" orm:"column(hostname)"`
 	Available bool   `json:"available" orm:"column(available)"`
 }
 
