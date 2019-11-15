22e0e31b45862f834d464babe7e265fa861c7a47
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2019-11-15 16:37:48 +0200

    [EPMDEDP-3929] Fix displaying Status Info table on codebase Overview page
    Change-Id: I09d96d27b777424b7bcb784618f60126f042046d

--- models/query/action_log.go
+++ models/query/action_log.go
@@ -6,8 +6,8 @@ type ActionLog struct {
 	Id             int       `json:"id" orm:"column(id)"`
 	LastTimeUpdate time.Time `json:"last_time_update" orm:"column(updated_at)"`
 	UserName       string    `json:"user_name" orm:"column(username)"`
-	Message        string    `json:"message" orm:"column(detailed_message)"`
-	Action         string    `json:"action" orm:"column(action_message)"`
+	Message        string    `json:"message" orm:"column(action_message)"`
+	Action         string    `json:"action" orm:"column(action)"`
 	Result         string    `json:"result" orm:"column(result)"`
 }
 
