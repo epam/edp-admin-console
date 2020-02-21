epmd-edp/admin-console

[EPMDEDP-4583] Remove cd-pipeline from AC UI
Change-Id: I3e70b00f2189f739d5000c665102636bcfcadb86

controllers/application.go
@@ -18,11 +18,13 @@ package controllers
 
 import (
 	"edp-admin-console/context"
+	"edp-admin-console/controllers/helper"
 	"edp-admin-console/models/command"
 	"edp-admin-console/models/query"
 	"edp-admin-console/service"
 	"edp-admin-console/service/platform"
 	"edp-admin-console/util"
+	"edp-admin-console/util/auth"
 	"fmt"
 	"github.com/astaxie/beego"
 	"html/template"
@@ -56,10 +58,6 @@ const (
 
 func (c *ApplicationController) GetApplicationsOverviewPage() {
 	flash := beego.ReadFromRequest(&c.Controller)
-	if flash.Data["success"] != "" {
-		c.Data["Success"] = true
-	}
-
 	applications, err := c.CodebaseService.GetCodebasesByCriteria(query.CodebaseCriteria{
 		Type: query.App,
 	})
@@ -69,10 +67,16 @@ func (c *ApplicationController) GetApplicationsOverviewPage() {
 		return
 	}
 
+	if flash.Data["success"] != "" {
+		c.Data["Success"] = true
+	}
+	if flash.Data["error"] != "" {
+		c.Data["DeletionError"] = flash.Data["error"]
+	}
 	contextRoles := c.GetSession("realm_roles").([]string)
 	c.Data["EDPVersion"] = context.EDPVersion
 	c.Data["Username"] = c.Ctx.Input.Session("username")
-	c.Data["HasRights"] = isAdmin(contextRoles)
+	c.Data["HasRights"] = auth.IsAdmin(contextRoles)
 	c.Data["Codebases"] = applications
 	c.Data["Type"] = query.App
 	c.Data["VersioningTypes"] = c.VersioningTypes
@@ -169,7 +173,7 @@ func contains(a []string, x string) bool {
 func (c *ApplicationController) CreateApplication() {
 	flash := beego.NewFlash()
 	codebase := c.extractApplicationRequestData()
-	errMsg := ValidRequestData(codebase)
+	errMsg := helper.ValidRequestData(codebase)
 	if errMsg != nil {
 		log.Info("failed to validate request data", "message", errMsg.Message)
 		flash.Error(errMsg.Message)
@@ -177,7 +181,8 @@ func (c *ApplicationController) CreateApplication() {
 		c.Redirect("/admin/edp/application/create", 302)
 		return
 	}
-	LogRequestData(codebase)
+	ld := helper.CreateLogRequestData(codebase)
+	log.Info(ld.String())
 
 	createdObject, err := c.CodebaseService.CreateCodebase(codebase)
 
@@ -281,11 +286,4 @@ func (c *ApplicationController) extractApplicationRequestData() command.CreateCo
 	}
 	codebase.Username = c.Ctx.Input.Session("username").(string)
 	return codebase
-}
-
-func isAdmin(contextRoles []string) bool {
-	if contextRoles == nil {
-		return false
-	}
-	return util.Contains(contextRoles, beego.AppConfig.String("adminRole"))
 }

controllers/codebase_rest.go
@@ -17,6 +17,7 @@
 package controllers
 
 import (
+	"edp-admin-console/controllers/helper"
 	"edp-admin-console/models/command"
 	"edp-admin-console/models/query"
 	"edp-admin-console/service"
@@ -25,7 +26,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/validation"
 	"github.com/satori/go.uuid"
 	"net/http"
 	"path"
@@ -34,11 +34,6 @@ import (
 type CodebaseRestController struct {
 	beego.Controller
 	CodebaseService service.CodebaseService
-}
-
-type ErrMsg struct {
-	Message    string
-	StatusCode int
 }
 
 func (c *CodebaseRestController) Prepare() {
@@ -62,16 +57,9 @@ func (c *CodebaseRestController) GetCodebases() {
 	c.ServeJSON()
 }
 
-func isTypeAcceptable(getParam string) bool {
-	if _, ok := query.CodebaseTypes[getParam]; ok {
-		return true
-	}
-	return false
-}
-
 func getFilterCriteria(this *CodebaseRestController) (*query.CodebaseCriteria, error) {
 	codebaseType := this.GetString("type")
-	if codebaseType == "" || isTypeAcceptable(codebaseType) {
+	if codebaseType == "" || helper.IsTypeAcceptable(codebaseType) {
 		return &query.CodebaseCriteria{
 			Type: query.CodebaseTypes[codebaseType],
 		}, nil
@@ -113,13 +101,14 @@ func (c *CodebaseRestController) CreateCodebase() {
 		codebase.Name = path.Base(*codebase.GitUrlPath)
 	}
 
-	errMsg := ValidRequestData(codebase)
+	errMsg := helper.ValidRequestData(codebase)
 	if errMsg != nil {
 		log.Info("Failed to validate request data", "err", errMsg.Message)
 		http.Error(c.Ctx.ResponseWriter, errMsg.Message, http.StatusBadRequest)
 		return
 	}
-	LogRequestData(codebase)
+	ld := helper.CreateLogRequestData(codebase)
+	log.Info(ld.String())
 
 	createdObject, err := c.CodebaseService.CreateCodebase(codebase)
 
@@ -139,27 +128,6 @@ func (c *CodebaseRestController) CreateCodebase() {
 	location := fmt.Sprintf("%s/%s", c.Ctx.Input.URL(), uuid.NewV4().String())
 	c.Ctx.ResponseWriter.WriteHeader(200)
 	c.Ctx.Output.Header("Location", location)
-}
-
-func createErrorResponseBody(valid validation.Validation) []byte {
-	errJson, _ := json.Marshal(extractErrors(valid))
-	errResponse := struct {
-		Message string
-		Content string
-	}{
-		"Body of request are not valid.",
-		string(errJson),
-	}
-	response, _ := json.Marshal(errResponse)
-	return response
-}
-
-func extractErrors(valid validation.Validation) []string {
-	var errMap []string
-	for _, err := range valid.Errors {
-		errMap = append(errMap, fmt.Sprintf("Validation failed on %s: %s", err.Key, err.Message))
-	}
-	return errMap
 }
 
 func (c *CodebaseRestController) Delete() {

routers/router.go
@@ -20,6 +20,7 @@ import (
 	"edp-admin-console/context"
 	"edp-admin-console/controllers"
 	"edp-admin-console/controllers/auth"
+	cd_pipeline2 "edp-admin-console/controllers/cd-pipeline"
 	"edp-admin-console/filters"
 	"edp-admin-console/k8s"
 	"edp-admin-console/repository"
@@ -185,7 +186,7 @@ func init() {
 		EDPComponent:     ecs,
 	}
 
-	cpc := controllers.CDPipelineController{
+	cpc := cd_pipeline2.CDPipelineController{
 		CodebaseService:   codebaseService,
 		PipelineService:   pipelineService,
 		EDPTenantService:  edpService,
@@ -213,6 +214,7 @@ func init() {
 		beego.NSRouter("/codebase/:codebaseName/overview", &cc, "get:GetCodebaseOverviewPage"),
 		beego.NSRouter("/codebase", &cc, "post:Delete"),
 		beego.NSRouter("/stage", &cpc, "post:DeleteCDStage"),
+		beego.NSRouter("/cd-pipeline/delete", &cpc, "post:DeleteCDPipeline"),
 		beego.NSRouter("/codebase/:codebaseName/branch", &controllers.BranchController{BranchService: branchService, CodebaseService: codebaseService}, "post:CreateCodebaseBranch"),
 
 		beego.NSRouter("/library/overview", &lc, "get:GetLibraryListPage"),

epam/edp-admin-console

controllers/application.go
@@ -18,11 +18,13 @@ package controllers
 
 import (
 	"edp-admin-console/context"
+	"edp-admin-console/controllers/validation"
 	"edp-admin-console/models/command"
 	"edp-admin-console/models/query"
 	"edp-admin-console/service"
 	"edp-admin-console/service/platform"
 	"edp-admin-console/util"
+	"edp-admin-console/util/auth"
 	"fmt"
 	"github.com/astaxie/beego"
 	"html/template"
@@ -56,10 +58,6 @@ const (
 
 func (c *ApplicationController) GetApplicationsOverviewPage() {
 	flash := beego.ReadFromRequest(&c.Controller)
-	if flash.Data["success"] != "" {
-		c.Data["Success"] = true
-	}
-
 	applications, err := c.CodebaseService.GetCodebasesByCriteria(query.CodebaseCriteria{
 		Type: query.App,
 	})
@@ -69,10 +67,16 @@ func (c *ApplicationController) GetApplicationsOverviewPage() {
 		return
 	}
 
+	if flash.Data["success"] != "" {
+		c.Data["Success"] = true
+	}
+	if flash.Data["error"] != "" {
+		c.Data["DeletionError"] = flash.Data["error"]
+	}
 	contextRoles := c.GetSession("realm_roles").([]string)
 	c.Data["EDPVersion"] = context.EDPVersion
 	c.Data["Username"] = c.Ctx.Input.Session("username")
-	c.Data["HasRights"] = isAdmin(contextRoles)
+	c.Data["HasRights"] = auth.IsAdmin(contextRoles)
 	c.Data["Codebases"] = applications
 	c.Data["Type"] = query.App
 	c.Data["VersioningTypes"] = c.VersioningTypes
@@ -169,7 +173,7 @@ func contains(a []string, x string) bool {
 func (c *ApplicationController) CreateApplication() {
 	flash := beego.NewFlash()
 	codebase := c.extractApplicationRequestData()
-	errMsg := ValidRequestData(codebase)
+	errMsg := validation.ValidCodebaseRequestData(codebase)
 	if errMsg != nil {
 		log.Info("failed to validate request data", "message", errMsg.Message)
 		flash.Error(errMsg.Message)
@@ -177,7 +181,8 @@ func (c *ApplicationController) CreateApplication() {
 		c.Redirect("/admin/edp/application/create", 302)
 		return
 	}
-	LogRequestData(codebase)
+	ld := validation.CreateCodebaseLogRequestData(codebase)
+	log.Info(ld.String())
 
 	createdObject, err := c.CodebaseService.CreateCodebase(codebase)
 
@@ -281,11 +286,4 @@ func (c *ApplicationController) extractApplicationRequestData() command.CreateCo
 	}
 	codebase.Username = c.Ctx.Input.Session("username").(string)
 	return codebase
-}
-
-func isAdmin(contextRoles []string) bool {
-	if contextRoles == nil {
-		return false
-	}
-	return util.Contains(contextRoles, beego.AppConfig.String("adminRole"))
 }

controllers/codebase_rest.go
@@ -17,6 +17,7 @@
 package controllers
 
 import (
+	"edp-admin-console/controllers/validation"
 	"edp-admin-console/models/command"
 	"edp-admin-console/models/query"
 	"edp-admin-console/service"
@@ -25,7 +26,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/validation"
 	"github.com/satori/go.uuid"
 	"net/http"
 	"path"
@@ -34,11 +34,6 @@ import (
 type CodebaseRestController struct {
 	beego.Controller
 	CodebaseService service.CodebaseService
-}
-
-type ErrMsg struct {
-	Message    string
-	StatusCode int
 }
 
 func (c *CodebaseRestController) Prepare() {
@@ -62,16 +57,9 @@ func (c *CodebaseRestController) GetCodebases() {
 	c.ServeJSON()
 }
 
-func isTypeAcceptable(getParam string) bool {
-	if _, ok := query.CodebaseTypes[getParam]; ok {
-		return true
-	}
-	return false
-}
-
 func getFilterCriteria(this *CodebaseRestController) (*query.CodebaseCriteria, error) {
 	codebaseType := this.GetString("type")
-	if codebaseType == "" || isTypeAcceptable(codebaseType) {
+	if codebaseType == "" || validation.IsCodebaseTypeAcceptable(codebaseType) {
 		return &query.CodebaseCriteria{
 			Type: query.CodebaseTypes[codebaseType],
 		}, nil
@@ -113,13 +101,14 @@ func (c *CodebaseRestController) CreateCodebase() {
 		codebase.Name = path.Base(*codebase.GitUrlPath)
 	}
 
-	errMsg := ValidRequestData(codebase)
+	errMsg := validation.ValidCodebaseRequestData(codebase)
 	if errMsg != nil {
 		log.Info("Failed to validate request data", "err", errMsg.Message)
 		http.Error(c.Ctx.ResponseWriter, errMsg.Message, http.StatusBadRequest)
 		return
 	}
-	LogRequestData(codebase)
+	ld := validation.CreateCodebaseLogRequestData(codebase)
+	log.Info(ld.String())
 
 	createdObject, err := c.CodebaseService.CreateCodebase(codebase)
 
@@ -139,27 +128,6 @@ func (c *CodebaseRestController) CreateCodebase() {
 	location := fmt.Sprintf("%s/%s", c.Ctx.Input.URL(), uuid.NewV4().String())
 	c.Ctx.ResponseWriter.WriteHeader(200)
 	c.Ctx.Output.Header("Location", location)
-}
-
-func createErrorResponseBody(valid validation.Validation) []byte {
-	errJson, _ := json.Marshal(extractErrors(valid))
-	errResponse := struct {
-		Message string
-		Content string
-	}{
-		"Body of request are not valid.",
-		string(errJson),
-	}
-	response, _ := json.Marshal(errResponse)
-	return response
-}
-
-func extractErrors(valid validation.Validation) []string {
-	var errMap []string
-	for _, err := range valid.Errors {
-		errMap = append(errMap, fmt.Sprintf("Validation failed on %s: %s", err.Key, err.Message))
-	}
-	return errMap
 }
 
 func (c *CodebaseRestController) Delete() {

routers/router.go
@@ -20,6 +20,7 @@ import (
 	"edp-admin-console/context"
 	"edp-admin-console/controllers"
 	"edp-admin-console/controllers/auth"
+	cdPipeController "edp-admin-console/controllers/cd-pipeline"
 	"edp-admin-console/filters"
 	"edp-admin-console/k8s"
 	"edp-admin-console/repository"
@@ -185,7 +186,7 @@ func init() {
 		EDPComponent:     ecs,
 	}
 
-	cpc := controllers.CDPipelineController{
+	cpc := cdPipeController.CDPipelineController{
 		CodebaseService:   codebaseService,
 		PipelineService:   pipelineService,
 		EDPTenantService:  edpService,
@@ -213,6 +214,7 @@ func init() {
 		beego.NSRouter("/codebase/:codebaseName/overview", &cc, "get:GetCodebaseOverviewPage"),
 		beego.NSRouter("/codebase", &cc, "post:Delete"),
 		beego.NSRouter("/stage", &cpc, "post:DeleteCDStage"),
+		beego.NSRouter("/cd-pipeline/delete", &cpc, "post:DeleteCDPipeline"),
 		beego.NSRouter("/codebase/:codebaseName/branch", &controllers.BranchController{BranchService: branchService, CodebaseService: codebaseService}, "post:CreateCodebaseBranch"),
 
 		beego.NSRouter("/library/overview", &lc, "get:GetLibraryListPage"),