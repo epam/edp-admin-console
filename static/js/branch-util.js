epmd-edp/admin-console

[EPMDEDP-4792][EPMDEDP-4793] Add validation on startVersioningFrom field during app creation
Change-Id: I2d6066aa786b92f950e54c1c655f665206856d66

static/js/branch-util.js
@@ -1,0 +1,24 @@
+function handleBranchVersionValidation(branchVersion) {
+    let isValid = isBranchVersionValid(branchVersion),
+        elClass = branchVersion.attr("id");
+    if (!isValid) {
+        branchVersion.addClass('non-valid-input');
+        $('.invalid-feedback.' + elClass).show();
+    } else {
+        branchVersion.removeClass('non-valid-input');
+        $('.invalid-feedback.' + elClass).hide();
+    }
+    return isValid;
+}
+
+function isBranchVersionValid(branchVersion) {
+    if (branchVersion.val().length === 0) {
+        return false;
+    } else {
+        return !(!branchVersion.val() || !checkBranchVersion(branchVersion.val()));
+    }
+}
+
+function checkBranchVersion(branchVersion) {
+    return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|(?!.*RC|.*GA|.*SNAPSHOT)\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/i.test(branchVersion)
+}

epam/edp-admin-console

static/js/branch-util.js
@@ -1,0 +1,24 @@
+function handleBranchVersionValidation(branchVersion) {
+    let isValid = isBranchVersionValid(branchVersion),
+        id = branchVersion.attr("id");
+    if (!isValid) {
+        branchVersion.addClass('non-valid-input');
+        $('.invalid-feedback.' + id).show();
+    } else {
+        branchVersion.removeClass('non-valid-input');
+        $('.invalid-feedback.' + id).hide();
+    }
+    return isValid;
+}
+
+function isBranchVersionValid(branchVersion) {
+    if (branchVersion.val().length === 0) {
+        return false;
+    }
+
+    return !(!branchVersion.val() || !checkBranchVersion(branchVersion.val()));
+}
+
+function checkBranchVersion(branchVersion) {
+    return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|(?!.*RC|.*GA|.*SNAPSHOT)\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/i.test(branchVersion)
+}