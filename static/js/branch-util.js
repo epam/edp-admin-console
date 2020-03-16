c47496bb3c434050f1fa46d7e8e6997ffc8c64bc
Stanislav Kostenko <stanislav_kostenko@epam.com>
2020-03-16 16:15:12 +0200

    [EPMDEDP-4801] Adapt version validation regexp
    Change-Id: Icca7cab89260ae26243dbfc6a3c4c35ef7c8f89c

--- static/js/branch-util.js
+++ static/js/branch-util.js
@@ -20,9 +20,8 @@ function isBranchVersionValid(branchVersion) {
 }
 
 function checkBranchVersion(branchVersion) {
-    return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|(?!.*RC|.*GA|.*SNAPSHOT)\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/i.test(branchVersion)
+    return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)?$/i.test(branchVersion)
 }
-
 function trimMinorVersionComponent(version) {
     let components = version.split('.');
 
