epmd-edp/admin-console

[EPMDEDP-4937] add variable for test framework
Change-Id: I2a1fa6312970f288b0e4374640e8193b1b095a24

routers/router.go
@@ -33,6 +33,7 @@ import (
 const (
 	integrationStrategies = "integrationStrategies"
 	buildTools            = "buildTools"
+	testReport            = "testReport"
 	deploymentScript      = "deploymentScript"
 
 	CreateStrategy = "Create"
@@ -111,6 +112,11 @@ func init() {
 		log.Fatalf("buildTools config variable is empty.")
 	}
 
+	TestReport := util.GetValuesFromConfig(testReport)
+	if TestReport == nil {
+		log.Fatalf("testReport config variable is empty.")
+	}
+
 	ds := util.GetValuesFromConfig(deploymentScript)
 	if ds == nil {
 		log.Fatalf("deploymentScript config variable is empty.")
@@ -145,6 +151,7 @@ func init() {
 
 		IntegrationStrategies: util.RemoveElByValue(autis, CreateStrategy),
 		BuildTools:            buildTools,
+		TestReport:            TestReport,
 		DeploymentScript:      ds,
 	}
 

epam/edp-admin-console

Change-Id: Ib9b28777180a94f8cf718b2ba6709b1ce8f3801e

routers/router.go
@@ -31,9 +31,9 @@ import (
 	edpComponentService "edp-admin-console/service/edp-component"
 	"edp-admin-console/service/logger"
 	"edp-admin-console/util"
+	"fmt"
 	"github.com/astaxie/beego"
 	"go.uber.org/zap"
-	"fmt"
 )
 
 var log = logger.GetLogger()
@@ -42,6 +42,7 @@ const (
 	integrationStrategies = "integrationStrategies"
 	buildTools            = "buildTools"
 	versioningTypes       = "versioningTypes"
+	testReportTools       = "testReportTools"
 	deploymentScript      = "deploymentScript"
 
 	CreateStrategy = "Create"
@@ -139,6 +140,11 @@ func init() {
 		log.Fatal("versioningTypes config variable is empty.")
 	}
 
+	testReportTools := util.GetValuesFromConfig(testReportTools)
+	if testReportTools == nil {
+		log.Fatal("testReportTools config variable is empty.")
+	}
+
 	ds := util.GetValuesFromConfig(deploymentScript)
 	if ds == nil {
 		log.Fatal("deploymentScript config variable is empty.")
@@ -175,6 +181,7 @@ func init() {
 		IntegrationStrategies: util.RemoveElByValue(autis, CreateStrategy),
 		BuildTools:            buildTools,
 		VersioningTypes:       vt,
+		TestReportTools:       testReportTools,
 		DeploymentScript:      ds,
 	}
 