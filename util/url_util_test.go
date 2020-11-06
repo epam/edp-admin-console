b976c336b71c794ee6e77fd5498107cb53f2a0ea
Oleksandr Frolov <Oleksandr_Frolov@epam.com>
2020-11-06 12:13:58 +0200

    [EPMDEDP-5706]: Add unit tests for new project and image stream links
    Change-Id: I1745737db117772287651ac60eeca820e20037b2

--- util/url_util_test.go
+++ util/url_util_test.go
@@ -1,9 +1,22 @@
 package util
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+func TestCreateNativeProjectLinkMethod_ShouldBeExecutedSuccessfully(t *testing.T) {
+	beego.AppConfig.Set("projectMaskUrl", "/console/project/{namespace}/overview")
+	link := CreateNativeProjectLink("https://stub-domain", "stub-project")
+	assert.Equal(t, "https://stub-domain/console/project/stub-project/overview", link)
+}
+
+func TestCreateNativeDockerStreamLinkMethod_ShouldBeExecutedSuccessfully(t *testing.T) {
+	beego.AppConfig.Set("imageStreamMaskUrl", "/console/project/{namespace}/browse/images/{stream}")
+	link := CreateNativeDockerStreamLink("https://stub-domain", "stub-project", "stub-stream")
+	assert.Equal(t, "https://stub-domain/console/project/stub-project/browse/images/stub-stream", link)
+}
 
 func TestCreateGitlabCILinkMethod_ShouldBeExecutedSuccessfully(t *testing.T) {
 	link := CreateGitlabCILink("stub-domain", "stub-relative-path")
